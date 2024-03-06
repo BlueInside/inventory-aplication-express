bf63a2a97e4c5c48189fd5a991e20d18e7879e62
Karol <kpulawski13@gmail.com>
2024-03-06 12:49:36 +0000

    changed category to array of categories

--- models/item.js
+++ models/item.js
@@ -7,9 +7,9 @@ const { Schema } = mongoose;
 const ItemSchema = new Schema({
   name: { type: String, required: true, maxLength: 200 },
   description: { type: String, required: true },
-  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+  category: [{ type: Schema.Types.ObjectId, ref: 'Category', required: true }],
   price: { type: mongoose.Decimal128, required: true },
-  numberInStock: { type: Number, min: 0 },
+  numberInStock: { type: Number, min: 0, required: true },
 });
 
 // Virtual for item's URL

df7729c64c41289da42295032df3dc6457a86b03
Karol <kpulawski13@gmail.com>
2024-03-06 11:22:49 +0000

    defined item Schema

---
+++ models/item.js
@@ -0,0 +1,20 @@
+// Require mongoose
+const mongoose = require('mongoose');
+
+// Define a schema
+const { Schema } = mongoose;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true, maxLength: 200 },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+  price: { type: mongoose.Decimal128, required: true },
+  numberInStock: { type: Number, min: 0 },
+});
+
+// Virtual for item's URL
+ItemSchema.virtual('url').get(function () {
+  return `/items/${this._id}`;
+});
+
+module.exports = mongoose.model('Item', ItemSchema);

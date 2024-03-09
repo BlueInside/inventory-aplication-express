80745415ee518030a6c64949394767294f01b49a
Karol <kpulawski13@gmail.com>
2024-03-09 11:35:08 +0000

    added helmet to middleware chain

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require('compression');
+const helmet = require('helmet');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -30,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
